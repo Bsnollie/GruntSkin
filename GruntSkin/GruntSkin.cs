c7da9d8fdc3b09164fea7692184a74a0c4d73b0d
Rodrigo Soriano <rodrigo_skate9@hotmail.com>
2021-11-03 22:00:09 -0600

    Add project files.

---
+++ GruntSkin/GruntSkin.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.IO;
+
+namespace DuckGame.GruntSkin
+{
+    public class GruntSkin : ClientMod
+    {
+        public override Priority priority
+        {
+            get
+            {
+                return Priority.HatPack;
+            }
+        }
+
+        protected override void OnPostInitialize()
+        {
+            Team team = Team.Deserialize(Mod.GetPath<GruntSkin>("hats/Grunt MC.png"));
+
+            if (team != null)
+            {
+                Teams.AddExtraTeam(team);
+            }
+
+            base.OnPostInitialize();
+        }
+    }
+}
---
+++ GruntSkin/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+// The title of your mod, as displayed in menus
+[assembly: AssemblyTitle("|GRAY|[CLIENT] |255,255,0|Grunt Skin")]
+
+// The author of the mod
+[assembly: AssemblyCompany("Bsnollie")]
+
+// The description of the mod
+[assembly: AssemblyDescription("Punch, shoot and die like a Grunt from Madness Combat!")]
+
+// The mod's version
+[assembly: AssemblyVersion("1.0.0.3")]
